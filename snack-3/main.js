93dc4883c78657272badd4fc87280f63542bbb4e
ManuelTrappolini <manuel.trappolini@gmail.com>
2025-03-12 12:57:00 +0100

    add snack-3

---
+++ snack-3/main.js
@@ -0,0 +1,72 @@
+
+//Creare un array (authors) che contiene gli autori dei libri.
+//Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+//Ordina l’array authors in base all’età, senza creare un nuovo array.
+//(se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+
+const books = [
+    {
+        title: "React Billionaire",
+        pages: 250,
+        author: {
+            name: 'Alice',
+            age: 35
+        },
+        available: false,
+        price: '101€',
+        tags: ['advanced', 'js', 'react', 'senior']
+    },
+    {
+        title: "Advanced JS",
+        pages: 500,
+        author: {
+            name: 'Bob',
+            age: 20
+        },
+        available: true,
+        price: '25€',
+        tags: ['advanced', 'js', 'mid-senior']
+    },
+    {
+        title: "CSS Secrets",
+        pages: 320,
+        author: {
+            name: 'Alice',
+            age: 17
+        },
+        available: true,
+        price: '8€',
+        tags: ['html', 'css', 'junior']
+    },
+    {
+        title: "HTML Mastery",
+        pages: 200,
+        author: {
+            name: 'Charlie',
+            age: 50
+        },
+        available: false,
+        price: '48€',
+        tags: ['html', 'advanced', 'junior', 'mid-senior']
+    },
+];
+
+
+authors = books.map(books => books.author)
+console.log('authors:', authors);
+
+const areAuthorsAdults = authors.filter(author => author.age >= 18)
+console.log('areAuthorsAdults:', areAuthorsAdults);
+
+if (areAuthorsAdults.length === authors.length) {
+
+    authors.sort((a, b) => {
+        return a.age - b.age
+    })
+} else {
+    authors.sort((a, b) => {
+        return b.age - a.age
+    })
+}
+
+console.log('sorted authors:', authors);
